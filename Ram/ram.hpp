b999338bc0b3cbcad9d6c4095d8c4d6e42e7b096
Vasily Markov <mvv1993Vasya@gmail.com>
2024-10-01 20:19:45 +0300

    fix

--- Ram/ram.hpp
+++ Ram/ram.hpp
@@ -1,138 +1,190 @@
 #ifndef RAM_H
 #define RAM_H
 
-#include <string>
-#include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <memory>
+#include <numeric>
+#include "token.hpp"
 
 namespace my_impl {
 
-enum class token_type {
-    ADD,
-    SUB,
-    NUM,
-    OBRAC,
-    CBRAC,
-    ASSIGN,
-    INPUT,
-    OUTPUT,
-    SCOLON
-};
-
-class iToken {
-protected:
-public:
-    iToken() = default;
-    virtual ~iToken() = default;
-    virtual std::string toString() const = 0;
-};
-
-class AddToken: public iToken {
-public:
-    AddToken(): iToken() {}
-    std::string toString() const override {
-        return "+";
+template <typename T, typename U>
+inline void print(const std::unordered_map<T, U>& map) {
+    for(auto&& [key, value] : map) {
+        std::cout << "key: " << key 
+        << " with value :" << value <<  ' ';
     }
-};
+}
 
-class SubToken: public iToken {
-public:
-    SubToken(): iToken() {}
-    std::string toString() const override {
-        return "-";
-    }
-};
-
-class ValueToken: public iToken {
-public:
-    explicit ValueToken(int value) noexcept:iToken(), value_(value) {}
-    std::string toString() const override {
-        return std::to_string(value_);
-    }
-private:
-    int value_;
-};
-
-class IdToken: public iToken {
-public:
-    IdToken(std::string id): iToken(), id_(id) {}
-    std::string toString() const override {
-        return id_;
-    }
-private:
-    std::string id_;
-};
-
+inline std::unordered_map<std::string, int> var_store_;
+inline std::vector<int> global_memory_;
 
 class RamParser final {
 public:
-    RamParser(){}
-
-    void pushToken(token_type token) 
+    RamParser()
     {
-        switch (token)
-        {
-        case token_type::ADD:
-            tokens_.emplace_back(std::make_unique<AddToken>());
-            break;
-        case token_type::SUB:
-            tokens_.emplace_back(std::make_unique<SubToken>());
-            break;
-        case token_type::OBRAC:
-            
-            break;
-        case token_type::CBRAC:
-            
-            break;
-        case token_type::ASSIGN:
-            
-            break;
-        case token_type::SCOLON:
-            
-            break;
-        case token_type::INPUT:
-            
-            break;
-        case token_type::OUTPUT:
-            
-            break;
-        default:
-            break;
-        }
+        global_memory_.resize(1000);
+        std::iota(std::begin(global_memory_), std::end(global_memory_), 0);
     }
 
-    void pushValue(const char* value) 
+    void pushToken(std::unique_ptr<iToken> token) 
     {
-        tokens_.emplace_back(std::make_unique<ValueToken>(std::stoi(std::string(value))));
-    }
-
-    void pushId(const char* id) 
-    {
-        tokens_.emplace_back(std::make_unique<IdToken>(id));
+        tokens_.emplace_back(std::move(token));
     }
 
     void parse() {
-        parseToken(**std::begin(tokens_));
+        // for(auto&& token : tokens_) {
+        //     std::cout << token->toString() << std::endl;
+        // }
+        c_it_ = std::cbegin(tokens_);
+        statement();
     }
 
     friend std::ostream& operator<<(std::ostream& os, const RamParser& parser) 
     {
-        // std::cout << "parser" << std::endl;
         for(auto&& token : parser.tokens_) {
             os << token->toString() << ' ';
         }
         return os;
     }
 private:
-    void parseToken(const iToken& token) {
+    using tokens_t =  std::vector<std::unique_ptr<iToken>>;
+    using tokens_cit = tokens_t::const_iterator;
+    tokens_t tokens_;
+    tokens_cit c_it_;
 
+    bool tokenEq(tokens_cit c_it, token_type rhs) const
+        {return (*c_it)->type() == rhs;};
+
+    bool tokenNotEq(tokens_cit c_it, token_type rhs) const
+        {return (*c_it)->type() != rhs;};
+
+    void nextToken() {++c_it_;};
+
+    int statement() {
+
+        if(c_it_ == std::cend(tokens_)) return 0;
+
+        switch ((*c_it_)->type())
+        {
+        case token_type::INPUT:
+            input();
+            break;
+        case token_type::OUTPUT:
+            output();
+            break;
+        case token_type::ID:
+            assign();
+            break;
+        default:
+            break;
+        }
+        return 1;
     }
-    std::vector<std::unique_ptr<iToken>> tokens_;
+
+    int input() {
+        nextToken();
+        if(tokenEq(c_it_, token_type::ID)) {
+
+            auto id = static_cast<IdToken&>(**c_it_).id();
+
+            if(tokenEq(c_it_, token_type::SCOLON)) {
+                int tmp = 0;
+                std::cout << "input: " << std::endl;
+                std::cin >> tmp;
+                var_store_.emplace(id, tmp);
+            }
+        }
+        nextToken();
+        return statement();
+    }
+
+    int output() {
+        nextToken();
+        if(tokenEq(c_it_, token_type::ID)) {
+
+            auto var = var_store_.at(static_cast<IdToken&>(**c_it_).id());
+
+            nextToken();
+            
+            if(tokenEq(c_it_, token_type::SCOLON)) {
+                std::cout << var << std::endl;
+            }
+        }
+        nextToken();
+        return statement();
+    }
+
+    int assign() {
+        auto id = static_cast<IdToken&>(**c_it_).id();
+        nextToken();
+
+        if(tokenEq(c_it_, token_type::ASSIGN)) {
+            if(var_store_.find(id) != std::end(var_store_)) {
+                nextToken();
+                var_store_.at(id) = expr();
+            }
+            else {
+                nextToken();
+                var_store_.emplace(id, expr());
+            }
+        }
+        return statement();
+    }
+
+    int expr() {
+        auto res = factor();
+        while(tokenNotEq(c_it_, token_type::SCOLON))
+        {
+            switch ((*c_it_)->type())
+            {
+            case token_type::ADD:
+                nextToken();
+                res += factor();
+                break;
+            case token_type::SUB:
+                nextToken();
+                res -= factor();
+                break;
+            default:
+                break;
+            }
+        }
+        nextToken();
+
+        return res;
+    }
+
+    int factor() {
+        auto res = 0;
+        switch ((*c_it_)->type())
+        {
+        case token_type::VALUE:
+            res = static_cast<ValueToken&>(**c_it_).value();
+            break;
+        case token_type::ID:
+            res = var_store_.at(static_cast<IdToken&>(**c_it_).id());
+            break;
+        default:
+            break;
+        }
+        nextToken();
+        return res;
+    }
+
 };
 
-
+inline std::unique_ptr<iToken> newAdd()                     {return std::make_unique<AddToken>();}
+inline std::unique_ptr<iToken> newSub()                     {return std::make_unique<SubToken>();}
+inline std::unique_ptr<iToken> newOBracket()                {return std::make_unique<ObracketToken>();}
+inline std::unique_ptr<iToken> newCBracket()                {return std::make_unique<CbracketToken>();}
+inline std::unique_ptr<iToken> newAssign()                  {return std::make_unique<AssignToken>();}
+inline std::unique_ptr<iToken> newScolon()                  {return std::make_unique<ScolonToken>();}
+inline std::unique_ptr<iToken> newInput()                   {return std::make_unique<InputToken>();}
+inline std::unique_ptr<iToken> newOutput()                  {return std::make_unique<OutputToken>();}
+inline std::unique_ptr<iToken> newVal(const char* value)    {return std::make_unique<ValueToken>(value);}
+inline std::unique_ptr<iToken> newId(const char* id)        {return std::make_unique<IdToken>(id);}
 
 
 
---
+++ Ram/token.hpp
@@ -0,0 +1,160 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <iostream>
+
+
+namespace my_impl
+{
+
+enum class token_type {
+    ADD,
+    SUB,
+    VALUE,
+    OBRAC,
+    CBRAC,
+    ASSIGN,
+    INPUT,
+    OUTPUT,
+    SCOLON,
+    ID
+};
+
+class iToken {
+public:
+    virtual token_type type() const noexcept = 0;
+    virtual ~iToken() = default;
+    virtual std::string toString() const = 0;
+};
+
+class AddToken: public iToken {
+public:
+    AddToken() {}
+
+    token_type type() const noexcept override 
+    {return token_type::ADD;}
+
+    std::string toString() const override {
+        return "+";
+    }
+};
+
+class SubToken: public iToken {
+public:
+    SubToken() {}
+
+    token_type type() const noexcept override 
+    {return token_type::SUB;}
+
+    std::string toString() const override {
+        return "-";
+    }
+};
+
+class ObracketToken: public iToken {
+public:
+    ObracketToken() {}
+
+    token_type type() const noexcept override 
+    {return token_type::OBRAC;}
+
+    std::string toString() const override {
+        return "-";
+    }
+};
+
+class CbracketToken: public iToken {
+public:
+    CbracketToken() {}
+
+    token_type type() const noexcept override 
+    {return token_type::CBRAC;}
+
+    std::string toString() const override {
+        return "-";
+    }
+};
+
+class AssignToken: public iToken {
+public:
+    AssignToken() {}
+
+    token_type type() const noexcept override 
+    {return token_type::ASSIGN;}
+
+    std::string toString() const override {
+        return "-";
+    }
+};
+
+class ValueToken: public iToken {
+public:
+    explicit ValueToken(const char* value) noexcept:
+        value_(std::stoi(std::string(value))) {}
+
+    int value() const noexcept {return value_;}
+
+    token_type type() const noexcept override 
+    {return token_type::VALUE;}
+
+    std::string toString() const override {
+        return std::to_string(value_);
+    }
+private:
+    int value_;
+};
+
+class IdToken: public iToken {
+public:
+    IdToken(std::string id):id_(id) {}
+
+    token_type type() const noexcept override 
+    {return token_type::ID;}
+
+    std::string id() const noexcept {return id_;}
+
+    std::string toString() const override {
+        return id_;
+    }
+private:
+    std::string id_;
+};
+
+class InputToken: public iToken {
+public:
+    InputToken() {}
+
+    token_type type() const noexcept override 
+    {return token_type::INPUT;}
+
+    std::string toString() const override {
+        return "input";
+    }
+};
+
+class OutputToken: public iToken {
+public:
+    OutputToken() {}
+
+    token_type type() const noexcept override 
+    {return token_type::OUTPUT;}
+
+    std::string toString() const override {
+        return "print";
+    }
+};
+
+class ScolonToken: public iToken {
+public:
+    ScolonToken() {}
+
+    token_type type() const noexcept override 
+    {return token_type::SCOLON;}
+
+    std::string toString() const override {
+        return ";";
+    }
+};
+
+} // namespace my_impl
+
+#endif //TOKEN_H
