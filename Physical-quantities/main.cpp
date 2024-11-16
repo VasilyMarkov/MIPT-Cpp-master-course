1d50e708cad0c24b7686c4aa506543095e92ca25
Vasily Markov <mvv1993Vasya@gmail.com>
2024-11-17 01:51:21 +0300

    proudct diff dimension units

--- Physical-quantities/main.cpp
+++ Physical-quantities/main.cpp
@@ -1,9 +1,10 @@
 #include "physical-quantities.hpp"
 
 int main() {
-    Force unit1(2.0);
-    Force unit2(3.0);
+    Speed unit1(2.0);
+    Current unit2(3.0);
     auto product = unit1*unit2;
     std::cout << typeid(product).name() << std::endl;
     std::cout << product.value << std::endl;
+    product.print();
 }
--- Physical-quantities/physical-quantities.hpp
+++ Physical-quantities/physical-quantities.hpp
@@ -5,10 +5,43 @@
 #include <type_traits>
 #include <array>
 #include <utility>
+#include <unordered_map>
+
+namespace 
+{
+
+// std::unordered_map<size_t, std::string> dimension_names = 
+// {
+//     {0, "m"},
+//     {1, "kg"},
+//     {2, "s"},
+//     {3, "С"}
+// }
+
+template<typename Tuple, std::size_t... Is>
+void print_tuple_impl(const Tuple& t, std::index_sequence<Is...>) {
+    ((std::cout << (Is == 0 ? "" : ", ") << std::get<Is>(t)), ...);
+}
+
+template<typename Tuple>
+void print_tuple(const Tuple& t) {
+    print_tuple_impl(t, std::make_index_sequence<std::tuple_size<Tuple>::value>{});
+}
 
 template<int... Args>
 struct Unit {
     static constexpr auto dims = std::make_tuple(Args...);
+};
+
+template<typename UnitT>
+struct Value {
+    using Unit = UnitT;
+    double value = 0.0;
+    explicit Value(double d): value(d) {}
+
+    void print() {
+        print_tuple(Unit::dims);
+    }
 };
 
 template<typename Tuple1, typename Tuple2, std::size_t... Is>
@@ -16,26 +49,34 @@ constexpr auto tuple_sum_impl(Tuple1 t1, Tuple2 t2, std::index_sequence<Is...>) 
     return std::make_tuple((std::get<Is>(t1) + std::get<Is>(t2))...);
 }
 
-template<typename UnitT>
-struct Value {
-    using Unit = UnitT;
-    double value = 0.0;
-    explicit Value(double d): value(d) {}
-};
-
-
 template<typename Tuple1, typename Tuple2>
 constexpr auto tuple_sum(Tuple1 t1, Tuple2 t2) {
-    return tuple_sum_impl(t1, t2, std::make_index_sequence<std::tuple_size<Tuple1>::value>{});
+    return tuple_sum_impl(t1, t2, std::make_index_sequence<std::tuple_size_v<Tuple1>>{});
+}
+
+template<typename Tuple, std::size_t N>
+constexpr auto pad_tuple(Tuple t) {
+    if constexpr (N == 0) {
+        return t;
+    } else {
+        return pad_tuple<decltype(std::tuple_cat(t, std::make_tuple(0))), N - 1>(std::tuple_cat(t, std::make_tuple(0)));
+    }
 }
 
 template<int... Args1, int... Args2>
-constexpr auto sumDims(Value<Unit<Args1...>>, Value<Unit<Args2...>>) {
+constexpr auto sum_dims(Value<Unit<Args1...>>, Value<Unit<Args2...>>) {
     constexpr auto t1 = std::make_tuple(Args1...);
     constexpr auto t2 = std::make_tuple(Args2...);
-    constexpr auto result_tuple = tuple_sum(t1, t2);
-    constexpr auto max_dimension = std::max(std::tuple_size_v<decltype(t1)>, std::tuple_size_v<decltype(t2)>);
 
+    constexpr std::size_t size1 = sizeof...(Args1);
+    constexpr std::size_t size2 = sizeof...(Args2);
+    constexpr std::size_t max_size = (size1 > size2) ? size1 : size2;
+
+    constexpr auto padded_t1 = pad_tuple<decltype(t1), max_size - size1>(t1);
+    constexpr auto padded_t2 = pad_tuple<decltype(t2), max_size - size2>(t2);
+
+    constexpr auto result_tuple = tuple_sum(padded_t1, padded_t2);
+   
     constexpr auto to_unit_impl = [=]<std::size_t... Is>(std::index_sequence<Is...>) {
         return Unit<std::get<Is>(result_tuple)...>{};
     };
@@ -43,16 +84,25 @@ constexpr auto sumDims(Value<Unit<Args1...>>, Value<Unit<Args2...>>) {
     return to_unit_impl(std::make_index_sequence<std::tuple_size_v<decltype(result_tuple)>>{});
 }
 
+} // namespace 
+
 template<int... Args1, int... Args2>
 constexpr auto operator*(Value<Unit<Args1...>> val1, Value<Unit<Args2...>> val2) {
-    return Value<decltype(sumDims(val1, val2))>(val1.value * val2.value);
+    return Value<decltype(sum_dims(val1, val2))>(val1.value * val2.value);
 }
 
 using Meter = Unit<1,0,0>;
-// using Speed = Value<Unit<1,0,-1>>;
-// using Acceleration = Value<Unit<1,0,-2>>;
-using Force = Value<Unit<1,1,-2>>;
+using Square = Unit<2,0,0>;
+using Mass = Unit<0,1,0>;
+using Second = Unit<0,0,1>;
+using Second2 = Unit<0,0,2>;
+using Charge = Unit<0,0,0,1>;
 
+using Speed = Value<Unit<1,0,-1>>;
+using Acceleration = Value<Unit<1,0,-2>>;
+using Force = Value<Unit<1,1,-2>>;
+using Pressure = Value<Unit<-1,1,-2>>;
+using Current = Value<Unit<0,0,-1,1>>;
 
 
 
