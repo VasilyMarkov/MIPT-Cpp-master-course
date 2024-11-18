7cefc87dc1b2fc81f3ebbdb77c0683cc655a9496
Vasily Markov <mvv1993Vasya@gmail.com>
2024-11-18 11:50:19 +0300

    fix

--- tuple/tuple.hpp
+++ tuple/tuple.hpp
@@ -9,29 +9,37 @@
 #include <boost/mpl/for_each.hpp>
 #include <boost/mpl/placeholders.hpp>
 #include <boost/mpl/size_t.hpp>
+#include <boost/type_traits/remove_reference.hpp>
+#include <boost/type_index.hpp>
+#include <boost/type_traits/add_pointer.hpp>
 
 namespace mpl = boost::mpl;
 
 template <typename T>
-struct sizeofType : mpl::size_t<sizeof(std::remove_reference_t<T>)> {};
+struct sizeofType : mpl::size_t<sizeof(typename boost::remove_reference<T>::type)> {};
 
 template <typename... Ts>
 struct Tuple {
-    using Vec = mpl::vector<std::type_identity<Ts>...>;
+    using Vec = mpl::vector<Ts...>;
     using sorted = typename mpl::sort<Vec, mpl::less<sizeofType<mpl::_1>, sizeofType<mpl::_2>>>::type;
 };
 
-struct printType {
-    template <typename T>
-    void operator()(T) const {
-        std::cout << typeid(T).name() << std::endl;
-    }
-};
+template <typename T>
+void printType() {
+    std::cout << boost::typeindex::type_id<T>().pretty_name() << std::endl;
+}
 
 template <typename Tuple>
 void printSortedTypes() {
     using SortedTypes = typename Tuple::sorted;
-    boost::mpl::for_each<SortedTypes>(printType());
+    boost::mpl::for_each<SortedTypes, boost::add_pointer<mpl::_1>>
+    (
+        []<typename T>(T)
+        {
+            printType<T>();
+        }
+    );
 }
+
 
 #endif //TUPLE_H
