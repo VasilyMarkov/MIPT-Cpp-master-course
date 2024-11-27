2c1677598db3702e4be1a89fc649135751a4e720
Vasily Markov <mvv1993Vasya@gmail.com>
2024-11-27 21:45:00 +0300

    fix

--- Physical-quantities/test/test.cpp
+++ Physical-quantities/test/test.cpp
@@ -7,7 +7,7 @@ TEST(Physical_quantities, test1) {
     Time t(4.0);
     auto U = d*m*t;
     
-    EXPECT_EQ(U.value, 24);
+    EXPECT_DOUBLE_EQ(U.value, 24.0);
     ASSERT_TRUE((std::is_same_v<decltype(U), Value<Unit<1,1,1>>>));
 }
 
@@ -16,7 +16,7 @@ TEST(Physical_quantities, test2) {
     Value<Unit<1,0,1>> value2(3.0);
     auto U = value1*value2;
     
-    EXPECT_EQ(U.value, 6);
+    EXPECT_DOUBLE_EQ(U.value, 6.0);
     ASSERT_TRUE((std::is_same_v<decltype(U), Value<Unit<1,0,1>>>));
 }
 
@@ -25,7 +25,7 @@ TEST(Physical_quantities, test3) {
     Resistance resistance(150.0);
     auto voltage = current * resistance;
     
-    EXPECT_EQ(voltage.value, 15);
+    EXPECT_DOUBLE_EQ(voltage.value, 15.0);
     ASSERT_TRUE((std::is_same_v<decltype(voltage), Value<Unit<2,1,-2,-1>>>));
 }
 
@@ -34,7 +34,7 @@ TEST(Physical_quantities, test4) {
     Value<Unit<1,0,0,1,1>> value2(3.0);
     auto U = value1 * value2;
     
-    EXPECT_EQ(U.value, 6);
+    EXPECT_DOUBLE_EQ(U.value, 6.0);
     ASSERT_TRUE((std::is_same_v<decltype(U), Value<Unit<2,0,2,1,1>>>));
 }
 
@@ -44,7 +44,7 @@ TEST(Physical_quantities, test5) {
     Resistance R(150.0);
     auto Q = I*I*R*t;
     
-    EXPECT_EQ(Q.value, 15);
+    EXPECT_DOUBLE_EQ(Q.value, 15.0);
     ASSERT_TRUE((std::is_same_v<decltype(Q), Energy>));
 }
 
@@ -54,8 +54,17 @@ TEST(Physical_quantities, test6) {
 
     auto a = F / m;
     
-    EXPECT_EQ(a.value, 5);
+    EXPECT_DOUBLE_EQ(a.value, 5.0);
     ASSERT_TRUE((std::is_same_v<decltype(a), Acceleration>));
+}
+
+TEST(Physical_quantities, test7) {
+    Value<Unit<>> value1(2.0);
+    Value<Unit<>> value2(3.0);
+    auto U = value1*value2;
+    
+    EXPECT_DOUBLE_EQ(U.value, 6.0);
+    ASSERT_TRUE((std::is_same_v<decltype(U), Value<Unit<>>>));
 }
 
 int main(int argc, char **argv) {
